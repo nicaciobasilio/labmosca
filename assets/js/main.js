c2b43ec3ca36e2c45746783e20ae10f7c6f7e710
Nicacio Basilio <nicacioufrn@gmail.com>
2023-02-23 04:43:11 -0300

    atualizando projetos

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,26 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+let swiperProjects = new Swiper(".projects__container", {
+    loop: true,
+    spaceBetween: 24,
+
+
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+            slidesPerView: 2,
+            spaceBetween: -56,
+        },
+        
+    },
+    mousewheel: true,
+    keyboard: true,
+});
