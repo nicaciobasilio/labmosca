e2ff40d8e4ca68660f7afbd567acb005a1911258
Nicacio Basilio <nicacioufrn@gmail.com>
2023-02-25 01:57:55 -0300

    finlizando todas as seções e animações

--- assets/js/main.js
+++ assets/js/main.js
@@ -142,3 +142,24 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('bg-header') 
+                        : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .projects__container, .footer__container`)
+sr.reveal(`.home__info div`, {delay: 600, origin: 'bottom', interval: 100})
+sr.reveal(`.skills__content:nth-child(1), contact__content:nth-child(1)`, {origin: 'left'})
+sr.reveal(`.skills__content:nth-child(2), contact__content:nth-child(2)`, {origin: 'right'})
+sr.reveal(`.qualification__content`, {interval: 100})
